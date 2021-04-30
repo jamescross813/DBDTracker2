286de8ac5bea81957ca8dedd2960c852bb84d983
jamescross <crossjames813@gmail.com>
2021-04-30 17:33:51 -0500

    session route setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,43 @@ class ApplicationController < Sinatra::Base
         erb :'/registrations/signup'
     end
 
+    post '/registrations/signup' do 
+      
+        @user = User.new(:username => params[:username], :password => params[:password])
+        
+        if @user.save
+            session[:user_id] = @user.id
+            binding.pry
+            redirect "/users/#{@user.id}"
+          else
+            redirect '/failure'
+          end
+    end
+
+    get '/failure' do
+        erb :failure
+    end
+
+    get '/login' do
+        erb :'/sessions/login'
+    end
+
+    post '/login' do
+      
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/failure'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to "/"
+    end
+
+
 end
