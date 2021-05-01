e39ce3468a63a5238fec7839e789a371807e4c8c
jamescross <crossjames813@gmail.com>
2021-05-01 00:52:07 -0500

    formatting fixes

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -10,25 +10,5 @@ class Helpers
         !!session[:user_id]
     end
 
-    def self.has_values?(session)
-        if UserKiller.find_by(:user_id => session[:user_id])
-            @userkiller = UserKiller.find_by(:user_id => session[:user_id])
-             
-            # KillerPerks.find_by
-        end
-    end
-
-    def self.killers(session)
-        @user = User.find_by(:id => session[:user_id])
-        @killer_ids = []
-        UserKiller.all.each do |uk|
-            while uk.user_id == @user.id
-                @killer_ids << uk.killer_id
-            end
-            
-        end
-        
-    end
-
     
 end
