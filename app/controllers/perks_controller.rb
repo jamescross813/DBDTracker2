96162988bb433a95b83f0310cf28dd3e15be8d86
jamescross <crossjames813@gmail.com>
2021-04-30 20:34:03 -0500

    perks controller setup

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -1,2 +1,32 @@
 class PerksController < ApplicationController
-end
+
+    get '/perks' do
+        @perks_survivor = []
+        @perks_killer = []
+        Perk.all.each do |perk|
+            
+        if perk.role == "Survivor"
+            @perks_survivor << perk        
+        else
+            @perks_killer << perk   
+        end
+    end
+        erb :'/perks/index'
+    end
+    
+    post'/perks/new' do
+    
+    end
+    
+    get '/perks/:id' do 
+        @perk = Perk.find(params[:id])
+        erb :'/perks/show'
+    end
+    
+    patch '/perks/:id/edit' do
+    end
+    
+    delete '/perks/:id/delete' do
+    end
+    
+    end
