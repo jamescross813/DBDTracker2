a6b28ef0a4bfcd05892054e848bb65fa68bf1fe7
jamescross813 <crossjames813@gmail.com>
2022-12-02 20:45:52 -0600

    pry added to bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class UsersController < ApplicationController
     get '/users/:id' do
         @user = User.find_by_id(session[:user_id])
-        @currencies = @user.currencies 
+        binding.pry
+        # @currencies = @user.currencies 
         @survivor_ids = @user.survivors
         @killer_ids = @user.killers
              
