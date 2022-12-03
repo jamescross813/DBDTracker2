48540c428253de6db6f6dc069daf8e5244024cae
jamescross813 <crossjames813@gmail.com>
2022-12-03 11:44:51 -0600

    seed file update

---
+++ db/seeds.rb
@@ -0,0 +1,34 @@
+trapper = Killer.create(name: "Trapper", 
+                        realm: "The MacMillan Estate", 
+                        power: "Bear Trap", 
+                        weapon: "The Cleaver", 
+                        speed: "115 % | 4.6 m/s",
+                        terror_radius: "32",
+                        height: "Tall",
+                        difficulty: "Easy",
+                        overview: "The Trapper is an area-control Killer, able to apply pressure across the Map by placing deadly Bear Traps for Survivors to step into.His personal Perks, Unnerving Presence, Brutal Strength, and Agitation, allow him to chase and carry Survivors more effectively.He is focused on physical attributes and making use of his strength and the Survivors' fear.",
+                        lore: "Evan MacMillan idolised his father. It wasn't just that he was heir to a great fortune, it was the way he ran the estate. Raised under his firm hand, Evan had taken to running the workforce with an iron hand. Production was always high and the MacMillan Estate prospered under father and son.\n\nAs Archie MacMillan's mental health slowly disintegrated, Evan protected him from the herd who wanted a piece of the fortune. No matter what his father asked of him, Evan would do.\n\nWhen Archie MacMillan finally snapped, Evan became his enforcer in what would become known as the worst mass murder in modern history. They never proved that Evan lead over a hundred men into those dark tunnels before detonating the explosives and sealing them to their fate.The tale of the MacMillan Estate is a tale of wealth and power gone very wrong.\n\nHow many victims fell to the hands of father and son is unknown. No record is ever made of what became of Evan MacMillan. His father is another unsolved puzzle, found trapped in the locked basement of his own warehouse - starved and abandoned.",
+                        dlc: "Base Game")    
+                        
+        dwight = Survivor.create(name: "Dwight Fairfield", 
+                        role: "Nervous Leader", 
+                        overview: "Dwight Fairfield is a nervous Leader, able to locate his allies and increase their effectiveness.\r\n\r\nHis personal Perks, Bond, Prove Thyself, and Leader, allow him to get in proximity of other Survivors and provide bonuses to their actions as well as his own.\r\n\r\nHe is skilled at finding others and working as a group. His Perks help him and others by keeping them together and alive.", 
+                        lore: "Dwight was geeky and scrawny through high school. He always wanted to be one of the cool kids, but somehow never had the charisma. He tried out for the football team but was cut, the basketball team didn't even take a look, and his grades were distinctly below average. One weekend, on a team-building exercise from his dead-end job, Dwight's boss led them deep into the woods before breaking out his family recipe moonshine. Dwight remembered taking the first sip before waking up late the next morning all alone. During the night, the others had abandoned him. Once again, the laughing stock of the community Dwight tried to hike his way out of the woods. That was the last anyone ever heard of Dwight Fairfield.\r\n\r\nDwight isn't the typical guy you think of when someone says \"Survivor\". He lacks that certain pizzazz and without his glasses, he's more or less blind. But as the sun sets and the woods come alive, Dwight clasps to his rat race life, making sure that he'll live to see another day even though something unimaginable is after him. Dwight won't stop. He'll survive no matter what. As others spent hours being seen in high school. He spent hours becoming invisible and avoiding danger. And it doesn't matter if it's dangers in the hallway or dangers in the woods. Survival is key. As other employees panic when terror infects them, Dwight makes use of his disturbing teen experience. The tables have now turned and now others need to follow to Dwight's firm directions if they are to survive as he knows how to disappear.",
+                        difficulty: "Easy",
+                        dlc: "Base Game")  
+                
+        perk = HTTParty.get("https://dbd-api.herokuapp.com/perks")
+        perk_data = JSON.parse(perk.body)
+
+                perk_data.each do |data|
+                        Perk.create(:perk_name => data["perk_name"], 
+                                :description => data["description"], 
+                                :teach_level => data["teach_level"], 
+                                :role => data ["role"], 
+                                :name => data["name"])  
+                                
+                        end 
+        
+      
+
+
