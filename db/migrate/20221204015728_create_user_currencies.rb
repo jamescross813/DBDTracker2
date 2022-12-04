72eb5cab1bf915de06cfc5f620551ccc912a593c
jamescross813 <crossjames813@gmail.com>
2022-12-03 20:00:23 -0600

    user currency table params

--- db/migrate/20221204015728_create_user_currencies.rb
+++ db/migrate/20221204015728_create_user_currencies.rb
@@ -1,4 +1,8 @@
 class CreateUserCurrencies < ActiveRecord::Migration[5.2]
   def change
+    create_table :user_currencies do |t|
+      t.integer :user_id
+      t.string :currency_name
+      t.integer :currency_amount
   end
 end
