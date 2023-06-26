38e12f149dddbbf493799e402807d8de78d0b378
jamescross813 <crossjames813@gmail.com>
2023-06-25 22:18:37 -0500

    working on scraping for trapper

--- db/scrape_seed.rb
+++ db/scrape_seed.rb
@@ -1,2 +1,13 @@
-killer_http = HTTParty.get("https://deadbydaylight.fandom.com/wiki/Evan_MacMillan")
-trapper = Killer.create()
+trapper_http = HTTParty.get("https://deadbydaylight.fandom.com/wiki/Evan_MacMillan")
+
+trapper = Killer.create(:name => data["name"], 
+                        :realm => data["realm"], 
+                        :power => data["power"], 
+                        :weapon => data["weapon"], 
+                        :speed => data["speed"],
+                        :terror_radius => data["terror_radius"],
+                        :height => data["height"],
+                        :difficulty => data["difficulty"],
+                        :overview => data["overview"],
+                        :lore => data["lore"],
+                        :dlc => data["dlc"])
